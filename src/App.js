c099e177da6a05d1c0f84aa45b4c93715dc5e5de
M Rizki <rizki1997mr@gmail.com>
2023-06-28 20:38:50 +0700

    edit app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I deployed my first app with Vercel! 🤘🚀🚀
         </p>
         <a
           className="App-link"
