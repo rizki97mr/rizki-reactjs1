a1d7be7a6dcef697a9c1f227b2f7feb7b1b539b0
M Rizki <rizki1997mr@gmail.com>
2023-07-01 05:37:50 +0700

    use props in FunctionalComponent

--- src/Materi/Komponen/pembahasan/FunctionalComponent.jsx
+++ src/Materi/Komponen/pembahasan/FunctionalComponent.jsx
@@ -1,2 +1,9 @@
-const FunctionalComponent = () => <h1>I deployed my first app with Vercel! 🤘🚀🚀</h1>;
+const FunctionalComponent = ({code}) => {
+    return (
+        <div>
+            <h1>I deployed my first app with Vercel! 🤘🚀🚀</h1>
+            <h2>Using {code}</h2>
+        </div>
+    )
+}
 export default FunctionalComponent;
