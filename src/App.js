a0c1d907a8741ed2987d70dac2d90f13ee2dcac5
M Rizki <rizki1997mr@gmail.com>
2023-07-01 02:22:40 +0700

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Komponen from "./Materi/Komponen";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I deployed my first app with Vercel! 🤘🚀🚀
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Komponen />
       </header>
     </div>
   );
