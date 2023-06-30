1007450bbf4f466217dc0f5f80a7b715a55c5c2a
M Rizki <rizki1997mr@gmail.com>
2023-07-01 02:21:27 +0700

    add index.jsx

---
+++ src/Materi/Komponen/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ClassComponent from "./pembahasan/ClassComponent";
+import FunctionalComponent from "./pembahasan/FunctionalComponent";
+
+
+
+ export default class Komponen extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <ClassComponent/>
+                <FunctionalComponent />
+            </div>
+        )
+    }
+}
