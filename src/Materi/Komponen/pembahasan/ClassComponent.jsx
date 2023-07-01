86813ceddcf280600b4c0529a7235b1a67924c6a
M Rizki <rizki1997mr@gmail.com>
2023-07-02 01:22:17 +0700

    edit classComponent

--- src/Materi/Komponen/pembahasan/ClassComponent.jsx
+++ src/Materi/Komponen/pembahasan/ClassComponent.jsx
@@ -1,12 +1,29 @@
 import React from "react";
 
 class ClassComponent extends React.Component {
-    
+    state = {
+        value: 0
+    }
+
+    handleMinus = () => {
+        if(this.state.value > 0) {
+            this.setState({value: this.state.value - 1})
+        }
+    }
+
+    handlePlus = () => {
+        this.setState({value: this.state.value + 1})
+    }
+
     render() {
         return(
             <div>
                 <h1>Hello World</h1>
                 <h2>I AM {this.props.nama} </h2>
+                <h4>Number</h4>
+                <button onClick={this.handleMinus}> - </button>
+                <span>{' '} {this.state.value} {' '}</span>
+                <button onClick={this.handlePlus}> +</button>
             </div>
         )
     }
