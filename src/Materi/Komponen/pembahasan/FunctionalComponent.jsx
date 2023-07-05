e43863ca1d0622ffbd3fe9134befe0eb72c25087
M Rizki <rizki1997mr@gmail.com>
2023-07-06 00:39:33 +0700

    useState in FunctionalComponent

--- src/Materi/Komponen/pembahasan/FunctionalComponent.jsx
+++ src/Materi/Komponen/pembahasan/FunctionalComponent.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
+
 const FunctionalComponent = ({code}) => {
+    const [value, setValue] = useState(0);
+
+    const handlePlus = () => {
+        setValue(value + 1);
+    }
+
+    const handleMinus = () => {
+        if(value > 0)
+        setValue(value - 1);
+    }
+
     return (
         <div>
+            <button onClick={handleMinus}>-</button>
+            <span>{' '}{value}{' '}</span>
+            <button onClick={handlePlus}>+</button>
             <h1>I deployed my first app with Vercel! 🤘🚀🚀</h1>
             <h2>Using {code}</h2>
         </div>
